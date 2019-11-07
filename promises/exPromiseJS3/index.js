c6fe10db1a7e5a67d96205eaf25e90409cb197b3
Lydia Rodrigues <mlydia295@gmail.com>
2019-11-07 11:30:46 -0300

    Exercicio com o promise da js3

--- promises/exPromiseJS3/index.js
+++ promises/exPromiseJS3/index.js
@@ -1,46 +1,38 @@
-function criaTexto(texto) {
-    const div = document.querySelector('#root');
+function criaTexto(texto){
+    const div = document.querySelector('#root')
     div.innerHTML += `<h1>${texto}</h1>`
-  }
-  
-  const sabao = true;
-  const agua = true;
-  
-  const ensaboar = () => {
+}
+
+const sabao = true
+const agua = true
+
+const ensaboar = () => {
     return new Promise((resolve, reject) => {
-      if (sabao) {
-        setTimeout(() => {
-          resolve('terminou de ensaboar');
-        }, 5000);
-      } else {
-        reject('não consigo ensaboar');
-      }
+        if(sabao){
+            setTimeout(() => {
+                resolve('terminou de ensaboar');
+            }, 5000);
+        } else {
+            reject('não consigo ensaboar')
+        }
     })
-  }
-  
-  function enxaguar() {
+}
+
+const enxaguar = () => {
     return new Promise((resolve, reject) => {
-      if (agua) {
-        setTimeout(() => {
-          resolve('terminou de enxaguar');
-        }, 2000);
-      } else {
-        reject('não consigo enxaguar');
-      }
+        if (agua) {
+            setTimeout(() => {
+                resolve('terminou de enxaguar')
+            }, 6000)
+        } else {
+            reject('não consigo enxaguar')
+        }
     })
-  }
-  
-  ensaboar()
-    .then(res => {
-      criaTexto(res)
-      enxaguar()
-        .then(res2 => criaTexto(res2))
-        .catch(err2 => criaTexto(err2))
-    })
-    .catch(err => criaTexto(err))
-  
-  
-  
-  // Promise.all([ensaboar(), enxaguar()])
-  //   .then(resAll => resAll.forEach(res => criaTexto(res)))
-  //   .catch(err => console.log(err))
+}
+
+ensaboar().then( res => {
+    criaTexto(res)
+    return enxaguar()
+}).then( res2 => criaTexto(res2)).catch(err => criaTexto(err))
+
+Promise.all([ensaboar(), enxaguar()]).then(resAll => resAll.forEach(resUnica => criaTexto(resUnica))).catch(err => console.log(err))
