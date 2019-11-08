acd882cece68e9d7bea7fb06f425f6b5cb6afd24
Lydia Rodrigues <mlydia295@gmail.com>
2019-11-08 11:55:26 -0300

    Finalizando a Aula3

--- fetch/ex02/main.js
+++ fetch/ex02/main.js
@@ -1,14 +1,26 @@
 const cepInput = document.getElementById("cep")
 const logradouroInput = document.getElementById("logradouro")
-//... continuem a pegar os inputs do index.html para preencher depois com o valor da api
+const complementoInput = document.getElementById("complemento")
+const bairroInput = document.getElementById("bairro")
+const localidadeInput = document.getElementById("localidade")
+const ufInput = document.getElementById("uf")
 
 cepInput.addEventListener("blur", ()=>{
-    if(cepInput.input){
+    if(cepInput.value){
         fetch(`https://viacep.com.br/ws/${cepInput.value}/json`).then(response => {
             return response.json()
         }).then(json => {
             ///façam aparecer os valores nos inputs
-
+            preencheInputs(json)
         })
     }
 })
+
+
+preencheInputs = objeto => {
+    logradouroInput.value = objeto.logradouro
+    complementoInput.value = objeto.complemento
+    bairroInput.value = objeto.bairro
+    localidadeInput.value = objeto.localidade
+    ufInput.value = objeto.uf
+}
